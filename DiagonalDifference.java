f2cac43ec8195445667f0fb5ba33911d5a5f987a
A.k.R.Santosh <47202184+akrsantosh@users.noreply.github.com>
2022-07-01 23:07:46 +0530

    Update DiagonalDifference.java
    Now the code is working as per the logic

--- DiagonalDifference.java
+++ DiagonalDifference.java
@@ -1,70 +1,51 @@
 package HackerRankSolutions;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
- class Result  {
-
-    /*
-     * Complete the 'diagonalDifference' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
-     */
-
-    public static int diagonalDifference(List<List<Integer>> arr) {
-    	
-    	int res=0;
-    	
-    	for( int i=0; i<3;i++) {
-			
-			for(int j=0;j<3;j++) {
-			arr.get(i);
-			}
-			
-		}
-		return res;
-
-    }
-
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class DiagonalDifference {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<List<Integer>> arr = new ArrayList<>();
-
-        IntStream.range(0, n).forEach(i -> {
-            try {
-                arr.add(
-                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt)
-                        .collect(toList())
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-
-        int result = Result.diagonalDifference(arr);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		ArrayList<ArrayList<Integer>> Arr = new ArrayList();
+		
+		System.out.println("Please enter the size of Matrix : ");
+		int size = in.nextInt();
+		
+		
+		//Initilasition of an ArrayList
+				for( int i=0; i<size;i++) {
+					Arr.add(new ArrayList<>());
+					
+				}
+				
+				
+				//Adding Elements
+				for( int i=0; i<size;i++) {
+					
+					for(int j=0;j<size;j++) {
+						Arr.get(i).add(in.nextInt());
+					}
+					
+				}
+					System.out.println(Arr);
+					
+					int res = DiagonalDifference.diagonalDifference(Arr);
+					System.out.println(res);
+				
+	}
+	
+	
+	
+	 public static int diagonalDifference(ArrayList<ArrayList<Integer>> arr) {
+		   int leftdiagonal = 0, rightdiagonal = 0;
+		for(int i = 0, j = arr.get(0).size()-1; i < arr.get(0).size(); i++, j--){
+		        leftdiagonal = leftdiagonal + arr.get(i).get(i);
+		        rightdiagonal = rightdiagonal + arr.get(i).get(j);
+		    }
+		    return Math.abs(leftdiagonal - rightdiagonal);
+		    }
+    
 }
